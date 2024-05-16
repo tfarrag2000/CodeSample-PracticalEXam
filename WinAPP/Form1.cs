ce3bcaf1d0b64ae1bf33ef39a94d11f433f0ebc4
FHD <FHD@LAPTOP-F7H9JPUK>
2024-05-16 08:57:51 +0300

    exam

--- WinAPP/Form1.cs
+++ WinAPP/Form1.cs
@@ -6,5 +6,38 @@ namespace WinAPP
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string c5 = ("");
+            string c1 = textBox2.Text;
+            textBox1.Text = c1;
+            textBox2.Text = c5;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string c5 = ("");
+            string c2 = textBox1.Text;
+            textBox2.Text = c2;
+            textBox1.Text = c5;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string c2 = textBox1.Text;
+            string c3 = textBox2.Text;
+            textBox1.Text = c3;
+            textBox2.Text = c2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string c5 = ("");
+            textBox1.Text= c5;
+            textBox2.Text= c5;
+        }
     }
 }
